17435323a96b46429c851fddc87c39cface9d926
rsa1791 <prof.renatao@hotmail.com>
2020-12-05 17:50:32 -0300

    Atualizada a pasta blog
    #close2

---
+++ mysite/blog/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('publishe', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_lenght=250, unique_for_date='publish')
+    author = models.SlugField(User, on_delete=models.CASCADE, related_name='blog_post')
+    body = models.TextFiedl()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_lenght=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('publish',)
+
+    def __str__(self):
+        return self.title
