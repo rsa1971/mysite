b7abda93a087172befdbafabd80267ac5b60608c
rsa1791 <prof.renatao@hotmail.com>
2020-12-19 19:31:07 -0300

    Corrigir o urls.py do mysite/myiste/mysite.
    close #32.

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -14,23 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from . import views
-
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-
-app_name = 'blog'
-
-
-urlpatterns = [
-    # post views
-    path('', views.post_list, name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-         name='post_detail'),
-    path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls', namespace='blog')),
 ]
